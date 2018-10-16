4e99fdcaaae8b6aa4b4a0166d074d0715c896586
Venkatesh.pala <venkateshreddypala@gmail.com>
2018-10-15 23:49:54 -0400

    Update VectorAdd.py

--- VectorAdd.py
+++ VectorAdd.py
@@ -17,10 +17,10 @@ def VectorAdd(a,b):
         C = VectorAdd(A, B)
         vector_add_time = time.time() -start
 
-    print "C[:5] = " + str(C[:5])
-    print "C[-5:] = " + str(C[-5:])
+    print ("C[:5] = " + str(C[:5]))
+    print ("C[-5:] = " + str(C[-5:]))
 
-    print "VectorAdd took for % seconds" % vector_add_time
+    print ("VectorAdd took for % seconds" % vector_add_time)
 
 if __name__=='__main__':
     main()
